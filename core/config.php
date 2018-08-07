4d0509210783e26c22e3c7bbfba7039f40b36429
elinformatico <donoe1985@gmail.com>
2018-08-06 22:05:10 -0500

    fix: Se retiro el slash de la configuracion para funcione en produccion

--- core/config.php
+++ core/config.php
@@ -2,4 +2,4 @@
     define('DEVELOPMENT_SERVER',    'http://localhost');
     define('PRODUCTION_SERVER',     'http://proyecto.noehdez.info');
     define('ROOT_DEVELOPMENT',      '/fer');
-    define('ROOT_PRODUCTION',       '/');
+    define('ROOT_PRODUCTION',       '');
