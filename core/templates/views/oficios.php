6cd1b0275dec4b660b555033add852bfc4940781
J.Noe Hernandez Vivanco <donoe1985@gmail.com>
2018-10-29 18:50:07 -0400

    feature: Copiando funcionalidad de Memos para Oificios

--- core/templates/views/oficios.php
+++ core/templates/views/oficios.php
@@ -5,40 +5,68 @@
   </div>
 </div>
   <div class="container">
-    <form name='frmOficios' novalidate>
+    <form name='frmMemos' novalidate>
+
+        <!-- Turnado A -->
+        <div class="form-group">
+            <label class="text-label" for="tipoTurnadoA">Turnado a:</label>
+            <select class="form-control" id="tipoTurnadoA" ng-model='tipoTurnadoA' ng-change="fn.onChangeTurnadoA(tipoTurnadoA)">
+                <option value="" selected>-- Seleccione una opción --</option>
+                <option value="dependencia">Dependencia</option>
+                <option value="usuario">Usuario</option>
+                <option value="abierto">Abierto</option>
+            </select>
+        </div>
+    
+        <!-- Turnado a Dependencias -->
+        <div class="form-group" ng-show="mostrarTurnadoA_dependencia">
+            <label class="text-label" for="turnadoA_dependencia">Dependencia:</label>
+            <select class="form-control" id="turnadoA_dependencia" ng-model='turnadoA_dependencia' ng-change='fn.onChangeDependencia(turnadoA_dependencia)'>
+              <option value="" selected>-- Selecciona una Dependencia existente --</option>
+              <option ng-repeat="dependencia in dependencias" value="{{dependencia.id}}">{{dependencia.nombre}}</option>
+            </select>
+        </div>
+
+        <!-- Listado de Correspondencia para contestar  -->
+        <div class="form-group" ng-show="mostrarTurnadoA_dependencia">
+            <label class="text-label" for="turnadoA_dep_correspondencia">Correspondencia: </label>
+            <select class="form-control" id="turnadoA_dep_correspondencia" ng-model='turnadoA_dep_correspondencia'>
+              <option ng-repeat="correspondencia in correspondencias" value="{{correspondencia.id}}">{{correspondencia.referencia}}, {{correspondencia.descripcion}}</option>
+            </select>
+        </div>
+
+        <!-- Turnado a Usuario -->
+        <div class="form-group" ng-show="mostrarTurnadoA_usuario">
+            <label class="text-label" for="turnadoA_usuario">Turnado al Usuario:</label>
+            <select class="form-control" id="turnadoA_usuario" ng-model='turnadoA_usuario'>
+              <option value="" selected>-- Selecciona un Usuario--</option>
+                <option ng-repeat="usuario in usuarios" value="{{usuario.id}}">{{usuario.nombre}} --> [{{usuario.departamento}}]</option>
+            </select>
+        </div>
+
+        <!-- Turnado a Abierto -->
+        <div class="form-group" ng-show="mostrarTurnadoA_abierto">
+            <label class="control-label" for="txtTurnadoA_abierto">Turnado a: (Abierto)</label>
+            <input type="text" class="form-control" id="txtTurnadoA_abierto" ng-model='txtTurnadoA_abierto'>
+        </div>
 
         <div class="form-group">
-            <label class="text-label" for="oficiosTurnadoA">Turnado a:</label>
-            <select class="form-control" id="oficiosTurnadoA" ng-model='oficiosTurnadoA' required>
-              <option value="magna" selected>Opcion 1</option>
-              <option value="premium">Opcion 2</option>
+            <label class="text-label" for="tipoAnio">¿A que año esta dirigido?</label>
+            <select class="form-control" id="tipoAnio" ng-model='tipoAnio'>
+                <option value="" selected>-- Seleccione una opción --</option>
+                <option value="anio_corriente">Año Corriente</option>
+                <option value="anio_pasado">Año Pasado</option>
             </select>
         </div>
 
         <div class="form-group">
-            <div class="radio">
-                <label>
-                    <input type="radio" name="oficiosTipoAnio" ng-model="oficiosTipoAnio"> Año Corriente
-                </label>
-            </div>
+            <label class="control-label" for="txtAsunto">Asunto</label>
+            <input type="text" class="form-control" id="txtAsunto" ng-model='txtAsunto'>
         </div>
 
         <div class="form-group">
-            <div class="radio">
-                <label>
-                    <input type="radio" name="oficiosTipoAnio" ng-model="oficiosTipoAnio"> Año Pasado
-                </label>
-            </div>
-        </div>
-
-        <div class="form-group">
-            <label class="control-label" for="oficiosAsunto">Asunto</label>
-            <input type="text" class="form-control" id="oficiosAsunto" ng-model='oficiosAsunto' required>
-        </div>
-
-        <div class="form-group">
-            <label class="control-label" for="oficiosObservaciones">Observaciones</label>
-            <textarea class="form-control" rows="3" ng-model="oficiosObservaciones"></textarea>
+            <label class="control-label" for="txtObservaciones">Observaciones</label>
+            <textarea class="form-control" rows="3" ng-model="txtObservaciones"></textarea>
         </div>
         
         <div class="form-group">
@@ -46,7 +74,7 @@
                 type="button" 
                 class="btn btn-primary btn-lg" 
                 ng-click='fn.guardar()'>
-                Guardar Oficio
+                Guardar Memo
             </button>
         </div>
     </form>
--- js/controllers/oficiosCtrl.js
+++ js/controllers/oficiosCtrl.js
@@ -4,5 +4,162 @@ angular.module("mobieApp")
 function ( $rootScope,  $scope,  $http,  $compile,  $q,  $uibModal,  $log,  apiFactoryRest,  growlService ) {
 
     console.log('Controller --> oficiosCtrl');
-   
+    // Listados de Dependecias y Usuarios
+    $scope.dependencias = {};
+    $scope.usuarios = {};
+    $scope.correspondencias = {}; // Datos Auto generados
+    $scope.turnadoA_dep_correspondencia = '';
+
+   	$scope.fn = {
+   		init : function() {
+   			this.loadDependencias();
+   			this.loadUsuarios();
+   		},
+   		ocultarSubOpciones : function() {
+   			$scope.mostrarTurnadoA_dependencia = false;
+		    $scope.mostrarTurnadoA_usuario = false;
+		    $scope.mostrarTurnadoA_abierto = false;
+   		},
+   		onChangeTurnadoA : function(tipoTurnadoA) 
+   		{
+   			// console.log('OnChange: ', tipoTurnadoA);
+   			this.ocultarSubOpciones();
+
+   			if(tipoTurnadoA === 'dependencia') {
+   				$scope.mostrarTurnadoA_dependencia = true;
+
+   			} else if(tipoTurnadoA === 'usuario') {
+   				$scope.mostrarTurnadoA_usuario = true;
+
+   			} else if(tipoTurnadoA === 'abierto') {
+   				$scope.mostrarTurnadoA_abierto = true;
+   			}
+   		},
+   		onChangeDependencia : function(dependenciaId) 
+   		{
+   			console.log('onChangeDependencia: ', dependenciaId);
+
+   			// Limpiamos La lista
+   			$scope.correspondencias = {};
+
+   			if(dependenciaId !== undefined && dependenciaId !== '')
+   			{
+				apiFactoryRest.getCorrespondencias(dependenciaId)
+	             	.success(function(rs)
+	             	{
+	                    if(rs.status === 'success'){
+	                        $scope.correspondencias = rs.correspondencias;
+                            $scope.turnadoA_dep_correspondencia = dependenciaId;
+	                    }
+	                })
+	                .error(function(err){
+	                    growlService.error('Mensaje Sistema', err);
+                        $scope.turnadoA_dep_correspondencia = '';
+	                });
+   			} else {
+                $scope.turnadoA_dep_correspondencia = '';
+            }
+   		},
+   		loadDependencias : function() {
+   			apiFactoryRest.getDependencias()
+             	.success(function(rs)
+             	{
+                    if(rs.status === 'success'){
+                        $scope.dependencias = rs.dependencias;
+
+                    } else if(rs.status === 'error'){
+                        growlService.error('Mensaje Sistema', rs.msg);
+                    }
+                })
+                .error(function(err){
+                    growlService.error('Mensaje Sistema', err);
+                });
+   		},
+   		loadUsuarios : function() {
+   			apiFactoryRest.getNombreUsuarios()
+                .success(function(rs)
+                {
+                    if(rs.status === 'success'){
+                        $scope.usuarios = rs.usuarios;
+
+                    } else if(rs.status === 'error'){
+                        growlService.error('Mensaje Sistema', rs.msg);
+                    }
+                })
+                .error(function(err){
+                    growlService.error('Mensaje Sistema', err);
+                });
+   		},
+   		guardar : function() {
+
+   			console.log('Guardando datos..');
+   			$scope.datos = {
+                tipoRegistro : 'oficio',
+	   			tipoTurnadoA : $scope.tipoTurnadoA,
+	   			turnadoA_dep_correspondencia : $scope.turnadoA_dep_correspondencia,
+				turnadoA_usuario : $scope.turnadoA_usuario,
+				txtTurnadoA_abierto : $scope.txtTurnadoA_abierto,
+				tipoAnio : $scope.tipoAnio,
+				txtAsunto : $scope.txtAsunto,
+				txtObservaciones : $scope.txtObservaciones
+   			};
+
+   			// console.log($scope.datos);
+            
+            if(this.validarCampos()) {
+                apiFactoryRest.guardarMemoOficio($scope.datos)
+                  .success(function(rs)
+                    {
+                        if(rs.status === 'success')
+                        {    
+                            growlService.notice('Mensaje Sistema', rs.msg);
+                            $scope.fn.limpiarCampos();
+
+                        } else if(rs.status === 'error'){
+                            growlService.error('Mensaje Sistema', rs.msg);
+                        }
+                    })
+                    .error(function(err){
+                        growlService.error('Mensaje Sistema', err);
+                    });   
+            } else {
+                growlService.warning('Mensaje Sistema', '¡Por favor llene todos los campos para registrar el Oficio!');
+            }
+   		},
+        validarCampos : function() 
+        {
+            var firstValidation = false;
+            if($scope.tipoTurnadoA === 'dependencia'){
+                firstValidation = 
+                    ($scope.turnadoA_dependencia !== undefined && $scope.turnadoA_dependencia !== '') && 
+                    ($scope.turnadoA_dep_correspondencia !== undefined && $scope.turnadoA_dep_correspondencia !== '');
+                
+            } else if($scope.tipoTurnadoA === 'usuario') {
+                firstValidation = 
+                    ($scope.turnadoA_usuario !== undefined && $scope.turnadoA_usuario !== '');
+                
+            } else if($scope.tipoTurnadoA === 'abierto') {
+                firstValidation = 
+                    ($scope.txtTurnadoA_abierto !== undefined && $scope.txtTurnadoA_abierto !== '');
+            }
+            
+            return firstValidation && 
+                    ($scope.txtAsunto !== undefined && $scope.txtAsunto !== '') &&
+                    ($scope.txtObservaciones !== undefined && $scope.txtObservaciones !== '');
+        },
+   		limpiarCampos : function() 
+   		{
+			this.ocultarSubOpciones();
+   			$scope.tipoTurnadoA = '';
+            $scope.turnadoA_dependencia = '';
+			$scope.turnadoA_dep_correspondencia = '';
+			$scope.turnadoA_usuario = '';
+			$scope.txtTurnadoA_abierto = '';
+			$scope.tipoAnio = '';
+			$scope.txtAsunto = '';
+			$scope.txtObservaciones = '';
+   		}
+   	};
+
+   	$scope.fn.init();
 }]);
