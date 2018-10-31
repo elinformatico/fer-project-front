700999571d3b8ec24674e0c4a70830e857e0ada9
J.Noe Hernandez Vivanco <donoe1985@gmail.com>
2018-10-31 10:33:36 -0400

    feature: Agregando campos necesarios para la consulta de datos

--- core/templates/views/consultas.php
+++ core/templates/views/consultas.php
@@ -6,6 +6,55 @@
 </div>
   <div class="container">
     <form name='frmConsultas' novalidate>
+      
+        <div class="form-group">
+            <table class="table">
+                <tr>
+                    <td><label class="text-label" for="buscarPor">Buscar por: </label></td>
+                    <td>
+                        <select class="form-control" id="buscarPor" ng-model='buscarPor' ng-change="fn.onBuscarPor(buscarPor)">
+                            <option value="" selected>-- Seleccione una opción --</option>
+                            <option value="memos">Memos</option>
+                            <option value="oficios">Oficios</option>
+                            <?php if(getRoleSistema() !== 'basic' ): ?>
+                                <option value="correspondencia">Correspondencia</option>
+                            <?php endif; ?>
+                        </select>
+                    </td>
+                </tr>
+                <?php if(getRoleSistema() === 'admin' ): ?>
+                    <tr>
+                        <td><label class="text-label" for="buscarPor">Usuario: </label></td>
+                        <td>
+                            <div class="form-group">
+                                <select class="form-control" id="selectedUsuario" ng-model='selectedUsuario' required>
+                                    <option value="todos" selected>Todos los Usuario</option>
+                                    <option ng-repeat="usuario in usuarios" value="{{usuario.id}}">{{usuario.nombre}} --> [{{usuario.departamento}}]</option>
+                                </select>
+                            </div>
+                        </td>
+                    </tr>
+                <?php endif; ?>
+                <tr>
+                    <td><label class="text-label" for="buscarPor">Rango Fecha: </label></td>
+                    <td>
+                        <div class="form-group">
+                            <div class="input-daterange input-group rango-fecha" id="datepicker">
+                                <input 
+                                       type="text" 
+                                       class="input-sm form-control" 
+                                       name="start" ng-blur="fn.lostFocustInicial()" ng-model="fechaInicial" id="fechaInicial" />
+                                <span class="input-group-addon">to</span>
+                                <input 
+                                       type="text" 
+                                       class="input-sm form-control" 
+                                       name="end" ng-blur="fn.lostFocustFinal()" ng-model="fechaFinal" id="fechaFinal" />
+                            </div>
+                        </div>
+                    </td>
+                </tr>
+            </table>
+        </div>
         
         <div class="form-group">
             
--- js/controllers/consultasCtrl.js
+++ js/controllers/consultasCtrl.js
@@ -1,8 +1,38 @@
 angular.module("mobieApp")
 .controller("consultasCtrl", 
-         ["$rootScope","$scope","$http","$compile","$q","$uibModal","$log","apiFactoryRest","growlService",  
-function ( $rootScope,  $scope,  $http,  $compile,  $q,  $uibModal,  $log,  apiFactoryRest,  growlService ) {
+         ["$rootScope","$scope","$http","$compile","$q","$uibModal","$log","apiFactoryRest","growlService", "plugins",  
+function ( $rootScope,  $scope,  $http,  $compile,  $q,  $uibModal,  $log,  apiFactoryRest,  growlService, plugins ) {
 
     console.log('Controller --> consultasCtrl');
+    
+    $scope.usuarios = {};
+    $scope.selectedUsuario = 'todos';
+    
+    $scope.fechaInicial = '';
+    $scope.fechaFinal = '';
+  
+    $scope.fn = {
+        
+        init : function() {
+            console.log('Init');
+            plugins.createCalendar('.rango-fecha');
+        },
+        onBuscarPor : function(buscarPor) {
+            console.log('Buscando por: ', buscarPor);
+        },
+        lostFocustInicial : function() {
+            $scope.fechaInicial = plugins.getSelectedDate('#fechaInicial');
+            $scope.fechaFinal   = plugins.getSelectedDate('#fechaFinal');
+        }, 
+        lostFocustFinal : function() {
+            $scope.fechaInicial = plugins.getSelectedDate('#fechaInicial');
+            $scope.fechaFinal   = plugins.getSelectedDate('#fechaFinal');
+        },
+        consultar : function() {
+            console.log('Fecha Inicial: ', $scope.fechaInicial);
+            console.log('Fecha Final: ', $scope.fechaFinal);
+        }
+    };
    
+    $scope.fn.init();
 }]);
