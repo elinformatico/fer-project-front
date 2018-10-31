61b7c1c4189bc629b9e26d24f34a575e67137558
J.Noe Hernandez Vivanco <donoe1985@gmail.com>
2018-10-31 00:57:52 -0400

    fix: Se corrigio el formato de la pagina de acceso restringido

--- core/templates/views/restringido.php
+++ core/templates/views/restringido.php
@@ -1,1 +1,8 @@
-No tienes permiso para ver esta pagina xD
+<div class="container">
+  <br>
+  <div class="alert alert-danger" role="alert" style="text-align:center; margin-top: 100px; margin-bottom: 100px;">
+    <h2>
+      ¡Upsss! ¡No tienes permisos para ver esta página!
+    </h2>
+  </div>
+</div>
